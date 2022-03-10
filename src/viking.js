0aecc8de4f4ff14dee5187f961149b883ba2640d
Jessica Fernandez <jferasef@gmail.com>
2022-03-11 00:44:01 +0100

    bonus 4 finished

--- src/viking.js
+++ src/viking.js
@@ -53,7 +53,54 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack() {
+    let randomIndexViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let randomIndexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+    let viking = this.vikingArmy[randomIndexViking];
+    let saxon = this.saxonArmy[randomIndexSaxon];
+    let saxonStatus = saxon.receiveDamage(viking.strength);
+    if (saxonStatus === `A Saxon has died in combat`) {
+      this.saxonArmy.pop(randomIndexSaxon);
+    }
+    return saxonStatus;
+  }
+
+  saxonAttack() {
+    let randomIndexViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let randomIndexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+    let viking = this.vikingArmy[randomIndexViking];
+    let saxon = this.saxonArmy[randomIndexSaxon];
+    let vikingStatus = viking.receiveDamage(saxon.strength);
+    if (vikingStatus === `${viking.name} has died in act of combat`) {
+      this.vikingArmy.pop(randomIndexViking);
+    }
+    return vikingStatus;
+  }
+
+  showStatus() {
+    if ((this.saxonArmy = [])) {
+      return 'Vikings have won the war of the century!';
+    } else if ((this.vikingArmy = [])) {
+      return 'Saxons have fought for their lives and survived another day...';
+    } else if (this.vikingArmy > 1 && this.saxonArmy > 1) {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
